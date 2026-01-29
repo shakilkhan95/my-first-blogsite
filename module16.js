a2fd80950fc6d5cdae4dc77f9f0bec709946da4f
shakilkhan95 <th.shakilkhan@gmail.com>
2026-01-30 00:36:57 +0400

    module 16 practice task done

--- module16.js
+++ module16.js
@@ -1,13 +1,54 @@
-console.log('task2')
-//Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
-let a = 1;
-let sum = 0;
-while(a <= 200){
-    console.log(a);
-    sum = sum + a;
-    if(sum >= 100){
-        break;
+console.log('Task 1')
+//Access the golden rod color value in output.
+const colors = {
+    red: "#ff0000",
+    green: "#00ff00",
+    blue: "#0000ff",
+    "golden rod": '#daa520'
+};
+console.log(colors['golden rod']);
+
+console.log('Task2');
+//For this object below add a property named passenger capacity with value 5
+const car = {
+    make: "Toyota",
+    model: "Corolla",
+    year: 2020
+};
+car['passenger capacity'] = 5;
+console.log(car['passenger capacity']);
+
+console.log('Task 3');
+//Display the physics marks as output.
+const student = {
+    name: "Hamim Sakep",
+    id: 5421,
+    physics: {
+        subject: "HSC Physics",
+        author: "Shahjahan Tapan",
+        marks: 30
     }
-    a++
+};
+console.log(student.physics.marks);
+
+console.log('Task 4');
+//Count the number of properties.
+let students = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(Object.keys(students).length);
+
+console.log('Task 5');
+//Loop through an object and print the key-value pairs with their types
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+for(prop in myObject){
+    console.log(`key:${prop} | value:${myObject[prop]} | type: ${typeof myObject[prop]}`);
 }
-console.log(sum)
