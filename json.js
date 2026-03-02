4ac81d59378136974dd890aa7c7fc126ea1b87f2
shakilkhan95 <th.shakilkhan@gmail.com>
2026-03-02 15:11:19 +0400

    created some cards with json data which are automatically load while html loaded

--- json.js
+++ json.js
@@ -1,18 +1,28 @@
-const loadPosts = () => {
-    const url = "https://jsonplaceholder.typicode.com/posts#";
+document.addEventListener('DOMContentLoaded', () =>{
+    const cardContainer = document.getElementById("card-container");
+    
+    const url = "https://jsonplaceholder.typicode.com/posts";
     fetch(url)
     .then(res => res.json())
-    .then(data => showOnUi(data));
-}
+    .then(data => loadCard(data));
 
-const showOnUi = (posts) =>{
-    const container = document.getElementById('container');
-    for(const post of posts){
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <h1 style ="padding: 20px 0">${post.id}: ${post.title}</h1>    
-            <p style="color: #707070">${post.body}</p>
-        `;
-        container.appendChild(li);
+    function loadCard(cards){
+        for(const card of cards){
+            const div = document.createElement('div');
+            
+            div.innerHTML = `
+                <div class="card bg-primary text-primary-content w-96 mx-auto my-3">
+                    <div class="card-body">
+                        <h2 class="card-title capitalize">${card.title}</h2>
+                        <p>${card.body}</p>
+                        <div class="card-actions">
+                            <button class="btn">Click Here</button>
+                        </div>
+                    </div>
+                </div>
+            `;
+
+            cardContainer.appendChild(div);
+        }
     }
-}
+});
